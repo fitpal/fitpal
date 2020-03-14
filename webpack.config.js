4945853796b917481f79e896f9af2625144cfe16
Duygu Yigitsoy <duyguyigitsoy@gmail.com>
2020-03-14 14:09:01 -0400

    first edits

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+    entry: './client/index.js', 
+    output: {
+        path: path.resolve(__dirname, 'build'), 
+        filename: 'bundle.js'
+    }, 
+    mode: process.env.NODE_ENV, 
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /(node_modules|bower_components)/, 
+                use: {
+                    // babel-loader: compiles our application to translate newer version of browser language to be read in older versions 
+                    loader: 'babel-loader', 
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    },
+                },
+            
+            }, 
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            }
+          
+        ],
+    }, 
+
+    devServer: {
+        publicPath: '/build/',
+        contentBase: path.join(__dirname, 'client'),
+        port: 8080,
+        proxy: {
+            '/': 'http://localhost:3000'
+        }
+        
+    }
+}
