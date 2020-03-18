11115a321031761acb5748c84d35d5a436164c4f
Jimmy Chen <jimchn@jimmys-mbp.lan>
2020-03-17 21:49:24 -0400

    finished landing page and css

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -7,14 +7,14 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
+        <div className="dark-overlay landing-inner">
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">FitPal</h1>
                 <p className="lead"> made by Team Goblin Shark</p>
                 <hr />
-                <Link to="/login" className="btn btn-lg btn-primary mr-2">
+                <Link to="/login" className="btn btn-lg btn-dark mr-2">
                   Login
                 </Link>
                 <Link to="/signup" className="btn btn-lg btn-secondary">
